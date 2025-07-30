bb8bbceba3b59e74f453e7612b3aaf1508c0f64f
Isha <isha18919@gmail.com>
2025-07-30 22:49:09 +0530

    Develop Stats Section

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -27,3 +27,30 @@ export const allseeProducts = [
   "Apron",
   "O.T. Gown"
 ];
+
+export const stats =[
+  {
+    id: 0,
+    logo: "/assets/tshirt.svg",
+    numbers: "20",
+    span: "k+",
+    title: "Garments Produced",
+    description: "We produce over 20,000 garments every month, ensuring quality and style."
+  },
+  {
+    id: 1,
+    logo: "/assets/customers.svg",
+    numbers: "3",
+    span: "k+",
+    title: "Loyal Customers",
+    description: "Join our community of over 3,000 satisfied customers who trust us for their textile needs."
+  },
+  {
+    id: 2,
+    logo: "/assets/shining_star.svg",
+    numbers: "5",
+    span: "+",
+    title: "Years of Excellence",
+    description: "With 5+ years in the industry, we are committed to delivering excellence in every product."
+  },
+];
